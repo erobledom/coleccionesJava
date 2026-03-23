69a1faccb1401982892def2752cf8d1273876005
ericr <ericr@Solus.asir2.iestetuan.cloud>
2026-03-23 13:05:28 +0100

    Version primera de Eliza

--- src/ejeHashMap/eje04/Eliza.java
+++ src/ejeHashMap/eje04/Eliza.java
@@ -1,5 +1,41 @@
 package ejeHashMap.eje04;
+import java.util.HashMap;
+import java.util.Scanner;
+
 
 public class Eliza {
-
+	public static void main(String[] args) {
+		
+		var map = new HashMap <String, String> ();
+		
+		map.put("HOLA", "Hola que tal?");
+		map.put("ENCANTADO", "Encantado de conocerte, yo tambien");
+		map.put("ADIOS", "Adios espero volver a verte pronto");
+		map.put("HORA", "Lo siento, no llevo reloj");
+		map.put("NOMBRE", "Mi nombre es Eliza");
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println(" Bienvenido al sistema Eliza 0.0: ");
+		String frase;
+		boolean fin = false;
+		do {
+			frase = sc.nextLine();
+			for (String clave: map.keySet()) {
+				boolean encontrada = false;
+				if (frase.toUpperCase().contains(clave)) {
+					encontrada = true;
+					System.out.println(">"+map.get(clave));
+					if (frase.toUpperCase().contains("ADIOS")) {
+						fin = true;
+					}
+					break;
+				}
+			}
+		} while(! fin);
+			System.out.println(" Muchas gracias por conectarse");
+		
+		if (frase.toUpperCase().contains("HOLA")) {
+			
+		}
+	}
 }
